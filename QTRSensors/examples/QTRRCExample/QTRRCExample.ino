db37769402c78897a09268b5a1e4318e0c4b300f
Jamee (Pololu) <dev-jamee@pololu.com>
2013-04-18 14:04:13 -0700

    qtr_sensor_examples: fixed comments describing calibrated sensor values

--- QTRSensors/examples/QTRRCExample/QTRRCExample.ino
+++ QTRSensors/examples/QTRRCExample/QTRRCExample.ino
@@ -21,7 +21,7 @@
 // estimate the position of a line.  You can test this by taping a piece of 3/4" black
 // electrical tape to a piece of white paper and sliding the sensor across it.  It
 // prints the sensor values to the serial monitor as numbers from 0 (maximum reflectance) 
-// to 2500 (minimum reflectance) followed by the estimated location of the line as a number
+// to 1000 (minimum reflectance) followed by the estimated location of the line as a number
 // from 0 to 5000.  1000 means the line is directly under sensor 1, 2000 means directly
 // under sensor 2, etc.  0 means the line is directly under sensor 0 or was last seen by
 // sensor 0 before being lost.  5000 means the line is directly under sensor 5 or was
@@ -77,8 +77,8 @@ void loop()
   //  qtrrc.read(sensorValues); instead of unsigned int position = qtrrc.readLine(sensorValues);
   unsigned int position = qtrrc.readLine(sensorValues);
 
-  // print the sensor values as numbers from 0 to 9, where 0 means maximum reflectance and
-  // 9 means minimum reflectance, followed by the line position
+  // print the sensor values as numbers from 0 to 1000, where 0 means maximum reflectance and
+  // 1000 means minimum reflectance, followed by the line position
   for (unsigned char i = 0; i < NUM_SENSORS; i++)
   {
     Serial.print(sensorValues[i]);
